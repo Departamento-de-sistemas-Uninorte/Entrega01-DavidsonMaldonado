4d93c67f8cd44617da4845201c265ad84fe10599
Andrew <andrew11ds@hotmail.es>
2021-03-05 21:46:40 -0500

    Se cambio el correo ya que google bloquea el otro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'appserviciosback@gmail.com',
+    user_name:            'tempbackend01@gmail.com',
     password:             ENV['GMAIL_PASS'],
     authentication:       'plain',
     enable_starttls_auto: true }
